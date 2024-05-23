5ddb873a34cc508de2ce5a35408b0a716b1f7dee
hyungs0703 <hyungs0703@naver.com>
2024-05-23 15:24:31 +0900

    Query Methods란 무엇일까

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,7 +3,10 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
+//    //유저네임 입력하여 조회기능 추가
+//    List<Memo> findAllByUsername(String username);
 }
